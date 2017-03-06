526d58cd7ddcd5f5e077298ff37d2bdadd9076c5
Tyler James Devlin <devlintj@dcsdk12.org>
2017-03-06 12:49:04 -0700

    first commit

---
+++ src/PointOfSale.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class PointOfSale {
+	private Item[] items; 
+	private Reward[] rewards;
+	private Order order;
+	PointOfSale(String items, String rewards) {
+		loadItems(items);
+		loadRewards(rewards);
+	}
+	
+	private void loadItems(String items) {
+		Scanner scanner = new Scanner(new file(items));
+		String line = scanner.next();
+		
+		ArrayList<Item> items = new ArrayList<Item>();
+		
+		while (scanner.hasNext()) {
+			items.add(new Item(line));
+			line = scanner.next();
+		}
+	}
+	
+	private void loadRewards(String rewards) {
+		Scanner scanner = new Scanner(new file(rewards));
+		String line = scanner.next();
+		
+		ArrayList<Reward> rewards = new ArrayList<Reward>();
+		
+		while (scanner.hasNext()) {
+			rewards.add(new Reward(line));
+			line = scanner.next();
+		}
+	}
+	
+	public void displayTotal(Customer customer) {
+		int total = order.getPrice();
+		System.out.println("Your total is: " + total);
+	}
+	
+	private void clearOrder() {
+		order.clear();
+	}
+	
+	public Customer logIntoRewards(int rewardnum) {
+		for (Reward[] reward : rewards) {
+			if(reward.getNum(rewardnum) == customer.id) {
+				return reward.getCustomer;
+			}
+		}
+		return null;
+	}
+	
+	public void addItem(Item item) {
+		for(int i = 0; i < items.size(); i++) {
+			if(items[i].equals(item)) {
+				order.addItem(item);
+			}
+		}
+	}
+	
+	public void removeItem(Item item) {
+		order.remove(item);
+	}
+}
